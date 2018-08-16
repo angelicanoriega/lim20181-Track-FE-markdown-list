f84d085cb5fea21b52fd3d45910891fd5544c5b5
Maria <mariaanguevara@gmail.com>
2018-08-16 17:46:39 -0500

    close #5

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const  fs  = require ( 'fs' ) ; 
+console.log("hola");
+const [,, ...args]=process.argv;
+console.log("args",fs.access);
+
+console.log("args",args);
+
+console.log("1:path",args[0]);
+console.log("2:option",args[1]);
+
+console.log("argv",process.argv);
+
+
